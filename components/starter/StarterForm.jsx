ff2d167b93ecae14da47a5d6a368eb4e3444f707
Thomas Segismont <tsegismo@redhat.com>
2020-04-10 14:44:02 +0200

    Added fields to the starter form

--- components/starter/StarterForm.jsx
+++ components/starter/StarterForm.jsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import fetch from "isomorphic-unfetch";
+import Button from "../Button";
+import {Minus, Plus} from "react-feather";
 
 class StarterForm extends React.Component {
   constructor(props) {
@@ -7,7 +9,8 @@ class StarterForm extends React.Component {
     this.state = {
       error: null,
       isLoaded: false,
-      metadata: {}
+      metadata: {},
+      isAdvanced: false
     };
   }
 
@@ -30,8 +33,18 @@ class StarterForm extends React.Component {
         )
   }
 
+  radioButtons(values) {
+    return values.map(value => (
+        <span key={value}> {value} </span>
+    ));
+  }
+
+  toggleAdvanced() {
+    this.setState({isAdvanced: !this.state.isAdvanced});
+  }
+
   render() {
-    const {error, isLoaded, metadata} = this.state;
+    const {error, isLoaded, metadata, isAdvanced} = this.state;
 
     if (error) {
       return <div>Error : {error.message}</div>;
@@ -41,9 +54,47 @@ class StarterForm extends React.Component {
       return (
           <div>
             <div>
-              <span>Version: </span> {metadata.vertxVersions.map(version => (
-                <span> {version} </span>
-            ))}
+              <div><span>Version</span></div>
+              <div>{this.radioButtons(metadata.vertxVersions)}</div>
+            </div>
+            <div>
+              <div><span>Language</span></div>
+              <div>{this.radioButtons(metadata.languages)}</div>
+            </div>
+            <div>
+              <div><span>Build</span></div>
+              <div>{this.radioButtons(metadata.buildTools)}</div>
+            </div>
+            <div>
+              <div><span>Group Id</span></div>
+              <div><input type="text" defaultValue={metadata.defaults.groupId}/></div>
+            </div>
+            <div>
+              <div><span>Artifact Id</span></div>
+              <div><input type="text" defaultValue={metadata.defaults.artifactId}/></div>
+            </div>
+            <div>
+              <div><span>Dependencies</span></div>
+              <div>&nbsp;</div>
+            </div>
+            <div>
+              <a onClick={this.toggleAdvanced.bind(this)}><span>Advanced options {isAdvanced ? <Minus/> :
+                  <Plus/>}</span></a>
+            </div>
+            <div hidden={!isAdvanced}>
+              <hr/>
+              <div>
+                <div><span>Package</span></div>
+                <div><input type="text"/></div>
+              </div>
+              <div>
+                <div><span>JDK Version</span></div>
+                <div>{this.radioButtons(metadata.jdkVersions)}</div>
+              </div>
+              <hr/>
+            </div>
+            <div>
+              <a><Button primary>Generate project</Button></a>
             </div>
           </div>
       );
