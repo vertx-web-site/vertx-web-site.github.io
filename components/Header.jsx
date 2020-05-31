d810f48aad7359fce502013f06ba7d540137282d
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2020-05-31 15:46:23 +0200

    Add favicons

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,6 +11,10 @@ export default ({ title }) => (
       <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet"/>
       <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400" rel="stylesheet"/>
       <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400&display=swap" rel="stylesheet"/>
+      <link rel="shortcut icon" href="/favicons/favicon.ico"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="48x48" href="/favicons/favicon-48x48.png"/>
       <title>{title && title + " | "}Eclipse Vert.x</title>
     </Head>
     <NavBar />
