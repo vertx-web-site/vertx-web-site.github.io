40200e461e2c7a5697ddd1d05602cf3e53eec425
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2020-06-04 15:57:57 +0200

    Draft style for docs index

--- pages/docs/index.jsx
+++ pages/docs/index.jsx
@@ -1,57 +1,93 @@
 import Layout from "../../components/layouts/Page"
 import Link from "next/link"
-import { Fragment } from "react"
+import { Box, Database, Globe, Share2 } from "react-feather"
+import "./index.scss"
 
-const pages = {
-  "Core": {
-    "Java": "/docs/vertx-core/java/"
-  },
-  "Web":  {
-    "Java": "/docs/vertx-web/java/"
-  },
-  "Web Client":  {
-    "Java": "/docs/vertx-web-client/java/"
-  },
-  "PostgreSQL Client":  {
-    "Java": "/docs/vertx-pg-client/java/"
-  },
-  "MySQL Client":  {
-    "Java": "/docs/vertx-mysql-client/java/"
-  },
-  "DB2 Client":  {
-    "Java": "/docs/vertx-db2-client/java/"
-  },
-  "gRPC":  {
-    "Java": "/docs/vertx-grpc/java/"
+const Section = ({ icon, children }) => {
+  let numChildren = 1
+  if (Array.isArray(children)) {
+    numChildren = children.length
   }
+
+  numChildren = (numChildren - 1) % 3 + 1
+
+  return (
+    <section className="docs-index-section">
+      <div className="docs-index-section-header">
+        {icon}
+      </div>
+      <div className={`docs-index-section-content docs-index-section-content-${numChildren}`}>
+        {children}
+      </div>
+    </section>
+  )
 }
 
-export default () => {
-  let sections = []
+const SectionPart = ({ title, as, children }) => {
+  return (
+    <div className="docs-index-section-part">
+      <h3>
+        <Link href="/docs/[...slug]" as={as}>
+          <a>{title}</a>
+        </Link>
+      </h3>
 
-  for (let title in pages) {
-    let page = pages[title]
+      <p>{children}</p>
 
-    let links = []
-    for (let lang in page) {
-      let href = page[lang]
-      links.push(<li key={`${title}-${lang}`}><Link href="/docs/[...slug]" as={href}><a>{lang}</a></Link></li>)
-    }
+      <Link href="/docs/[...slug]" as={as}>
+        <a>Read</a>
+      </Link>
+    </div>
+  )
+}
 
-    sections.push(
-      <Fragment key={title}>
-        <h3>{title}</h3>
-        <ul>
-          {links}
-        </ul>
-      </Fragment>
-    )
-  }
-
+const Docs = () => {
   return (
     <Layout meta={{ title: "Documentation" }}>
       <h2>Documentation</h2>
-      {sections}
+
+      <Section icon={<Box className="feather" />}>
+        <SectionPart title="Core" as="/docs/vertx-core/java/">
+          Vert.x core contains fairly low level functionality including support
+          for HTTP, TCP, file system access, and various other features. You can
+          use it directly in your own applications. It&rsquo;s also used by many of
+          the other components of Vert.x.
+        </SectionPart>
+      </Section>
+
+      <Section icon={<Globe className="feather" />}>
+        <SectionPart title="Web" as="/docs/vertx-web/java/">
+          Vert.x-Web is a tool-kit for writing sophisticated modern web
+          applications and HTTP microservices.
+        </SectionPart>
+
+        <SectionPart title="Web Client" as="/docs/vertx-web-client/java/">
+          Vert.x Web Client is an easy-to-use client for HTTP and HTTP/2 with
+          a large number of advanced features.
+        </SectionPart>
+      </Section>
+
+      <Section icon={<Database className="feather" />}>
+        <SectionPart title="PostgreSQL Client" as="/docs/vertx-pg-client/java/">
+          A PostgreSQL client focusing on scalability and low overhead.
+        </SectionPart>
+
+        <SectionPart title="MySQL Client" as="/docs/vertx-mysql-client/java/">
+          A lightweight, event-driven client for MySQL.
+        </SectionPart>
+
+        <SectionPart title="DB2 Client" as="/docs/vertx-db2-client/java/">
+          TODO
+        </SectionPart>
+      </Section>
+
+      <Section icon={<Share2 className="feather" />}>
+        <SectionPart title="gRPC" as="/docs/vertx-grpc/java/">
+          Implement gRPC Clients and Servers for Vert.x.
+        </SectionPart>
+      </Section>
     </Layout>
   )
 }
+
+export default Docs
