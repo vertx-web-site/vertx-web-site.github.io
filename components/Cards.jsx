b8351e81ee70b50ff5e66fe733af0ded44b729c2
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2020-12-03 09:56:38 +0100

    Make cards responsive again

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -1,7 +1,8 @@
 import "./Cards.scss"
+import classNames from "classnames"
 
 const Cards = ({ columns = 1, children }) => {
-  return <div className="cards" style={{ gridTemplateColumns: `repeat(${columns}, 1fr)` }}>{children}</div>
+  return <div className={classNames("cards", `cards-cols-${columns}`)}>{children}</div>
 }
 
 export default Cards
