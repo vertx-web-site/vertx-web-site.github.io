3274aa7dcf50843684a15d10627db32c10d14d5a
okou19900722 <ilayan19900722@gmail.com>
2021-01-11 10:58:42 +0800

    打包时，不请求项目的star，访问时才请求

--- components/lib/github-stars.js
+++ components/lib/github-stars.js
@@ -1,8 +1,8 @@
 export async function fetchGitHubStars(org, repo) {
-  if (process.env.GITHUB_ACCESS_TOKEN === undefined) {
+  // if (process.env.GITHUB_ACCESS_TOKEN === undefined) {
     // do nothing
-    return undefined
-  }
+    return 0
+  // }
 
   const CACHE_TIMEOUT_SECONDS = 60 * 60 // one hour
   const CACHE_PATH = "./.cache/github_stars"
