a9c1993f785664a23ff6a75620574f94b91783ee
gcnyin <guchounongyin@gmail.com>
2021-02-09 14:21:20 +0800

    Updating docs/translation/vertx-reactive-streams/java/index.adoc

--- docs/metadata/3.9.0.jsx
+++ docs/metadata/3.9.0.jsx
@@ -253,8 +253,8 @@ const entries = [
   {
     id: "vertx-reactive-streams",
     name: "Reactive Streams",
-    description: `Reactive streams support so your applications can interoperate
-      with other reactive systems such as Akka and Project Reactor.`,
+    description: `对Reactive streams的支持。你的应用可以与其他 reactive systems 互操作，
+      比如Akka和Project Reactor`,
     category: "reactive",
     href: "/vertx-reactive-streams/java/",
     repository: "https://github.com/vert-x3/vertx-reactive-streams",
