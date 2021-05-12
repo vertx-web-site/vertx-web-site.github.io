c610d2f8fb2876c16b0043fd7a07fc63224dc086
Julien Viet <julien@julienviet.com>
2021-05-12 10:40:29 +0200

    Add open telemetry and HTTP proxy for 4.1.0

--- docs/metadata/4.1.0.jsx
+++ docs/metadata/4.1.0.jsx
@@ -3,6 +3,32 @@ import oldDocs from "./4.0.3"
 
 const docs = clone(oldDocs)
 
+insert(docs, "vertx-opentracing",
+    {
+        id: "vertx-opentelemetry",
+        name: "OpenTelemetry",
+        description: "Distributed tracing with OpenTelemetry.",
+        category: "monitoring",
+        href: "/vertx-opentelemetry/java/",
+        repository: "https://github.com/eclipse-vertx/vertx-tracing/tree/master/vertx-opentelemetry",
+        edit: "https://github.com/eclipse-vertx/vertx-tracing/tree/master/vertx-opentelemetry/src/main/asciidoc",
+        label: "Technical Preview"
+    }
+)
+
+insert(docs, "vertx-http-proxy",
+    {
+        id: "vertx-http-proxy",
+        name: "Vert.x Http Proxy",
+        description: "A reverse proxy based on Vert.x, it aims to implement reusable reverse proxy logic to focus on higher concerns.",
+        category: "web",
+        href: "/vertx-http-proxy/java/",
+        repository: "https://github.com/eclipse-vertx/vertx-http-proxy/tree/master",
+        edit: "https://github.com/eclipse-vertx/vertx-http-proxy/tree/master/src/main/asciidoc",
+        label: "Technical Preview"
+    }
+)
+
 // Mark this version as a prerelease. Prereleases can be selected manually
 // from the dropdown menu on the docs page but the default entry is still
 // the latest GA release.
