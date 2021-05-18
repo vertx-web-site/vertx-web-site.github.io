7c3802f0744d5496815d24ed85c15da50b5fe5e8
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2021-05-18 09:20:06 +0200

    Add commit SHA to HTML markup
    Fixes #109

--- next.config.js
+++ next.config.js
@@ -27,7 +27,8 @@ const config = {
   env: {
     basePath,
     // URL to the website. MUST NOT end with a slash.
-    baseUrl: isProd ? `https://vertx.io${basePath}` : `http://localhost:3000${basePath}`
+    baseUrl: isProd ? `https://vertx.io${basePath}` : `http://localhost:3000${basePath}`,
+    buildDate: new Date().toISOString()
   },
 
   // also render markdown pages
