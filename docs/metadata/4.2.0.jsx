eb58f8229b416defefdde13c23233d2cd71bde4c
Julien Viet <julien@julienviet.com>
2021-08-24 16:54:24 +0200

    Vert.x 4.2.0.Beta1 version on web site

---
+++ docs/metadata/4.2.0.jsx
@@ -0,0 +1,28 @@
+import { clone } from "./helpers"
+import oldDocs from "./4.1.0"
+import { insert } from "./helpers"
+
+const docs = clone(oldDocs)
+
+insert(docs, "vertx-mssql-client",
+     {
+         id: "vertx-oracle-client",
+         name: "Oracle Client",
+         description: "The Reactive Oracle client.",
+         category: "databases",
+         href: "/vertx-oracle-client/java/",
+         repository: "https://github.com/eclipse-vertx/vertx-oracle-client",
+         edit: "https://github.com/eclipse-vertx/vertx-sql-client/tree/master/vertx-oracle-client/src/main/asciidoc",
+         label: "Technical Preview"
+       }
+ )
+
+// Mark this version as a prerelease. Prereleases can be selected manually
+// from the dropdown menu on the docs page but the default entry is still
+// the latest GA release.
+docs.prerelease = true
+
+// force title
+docs.title = "4.2.0.Beta1"
+
+export default docs
