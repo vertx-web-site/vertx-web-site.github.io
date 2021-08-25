0d3a8ef3ad74321454d93f891f0f5fddb429e108
Paulo Lopes <pmlopes@gmail.com>
2021-08-25 14:37:08 +0200

    Adding vertx-auth-otp
    Signed-off-by: Paulo Lopes <pmlopes@gmail.com>

--- docs/metadata/4.2.0.jsx
+++ docs/metadata/4.2.0.jsx
@@ -17,6 +17,18 @@ insert(docs, "vertx-mssql-client",
        }
  )
 
+ insert(docs, "vertx-auth-webauthn",
+ {
+   id: "vertx-auth-otp",
+   name: "OTP Auth",
+   description: "One Time Password (Multi-Factor Authentication) implementation.",
+   category: "authentication-and-authorization",
+   href: "/vertx-auth-otp/java/",
+   repository: "https://github.com/vert-x3/vertx-auth/tree/master/vertx-auth-otp",
+   edit: "https://github.com/vert-x3/vertx-auth/tree/master/vertx-auth-otp/src/main/asciidoc"
+ }
+)
+
 // Mark this version as a prerelease. Prereleases can be selected manually
 // from the dropdown menu on the docs page but the default entry is still
 // the latest GA release.
