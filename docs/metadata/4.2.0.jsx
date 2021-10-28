18e88c44ef18324a215c2a53c4b711d6c1711553
Julien Viet <julien@julienviet.com>
2021-10-28 15:22:52 +0200

    Missing vertx-auth-webauthn

--- docs/metadata/4.2.0.jsx
+++ docs/metadata/4.2.0.jsx
@@ -17,4 +17,16 @@ insert(docs, "vertx-mssql-client",
        }
  )
 
+insert(docs, "vertx-auth-webauthn",
+  {
+    id: "vertx-auth-otp",
+    name: "OTP Auth",
+    description: "One Time Password (Multi-Factor Authentication) implementation.",
+    category: "authentication-and-authorization",
+    href: "/vertx-auth-otp/java/",
+    repository: "https://github.com/vert-x3/vertx-auth/tree/master/vertx-auth-otp",
+    edit: "https://github.com/vert-x3/vertx-auth/tree/master/vertx-auth-otp/src/main/asciidoc"
+  }
+ )
+
 export default docs
