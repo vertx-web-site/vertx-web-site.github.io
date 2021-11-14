91edadf1bf39135041f8f067e09e20f7743afc72
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2021-11-14 11:57:07 +0100

    Add progress bar to update-docs script

--- docs/src/download.js
+++ docs/src/download.js
@@ -4,9 +4,7 @@ const path = require("path")
 const stream = require("stream")
 const unzipper = require("unzipper")
 
-async function download(version) {
-  console.log(`Downloading documentation sources for version ${version} ...`)
-
+async function download(version, progressListener) {
   // TODO handle snapshots
   let url = `https://repo1.maven.org/maven2/io/vertx/vertx-stack-docs/${version}/vertx-stack-docs-${version}-docs.zip`
 
@@ -21,6 +19,8 @@ async function download(version) {
   let length = +res.headers.get("content-length")
   let lastProgress = 0
 
+  progressListener?.start(length)
+
   // TODO do we really need to save the zip file? Maybe enable this with a flag
   let zipFile = await fs.open(path.join(downloadPath, `vertx-stack-docs-${version}-docs.zip`), "w")
 
@@ -29,10 +29,9 @@ async function download(version) {
       .pipe(stream.Transform({
         transform(chunk, enc, cb) {
           downloaded += chunk.length
-          let progress = Math.round(downloaded * 100 / length)
-          if (progress > lastProgress && progress % 10 === 0) {
-            lastProgress = progress
-            console.log(`${version}\t${progress}%`)
+          if (downloaded - 1024 * 50 > lastProgress) {
+            progressListener?.update(downloaded)
+            lastProgress = downloaded
           }
           this.push(chunk)
           zipFile.write(chunk).then(() => cb()).catch(cb)
@@ -41,9 +40,7 @@ async function download(version) {
       .pipe(unzipper.Extract({ path: extractedPath }))
       .promise()
     await zipFile.close()
-    if (lastProgress !== 100) {
-      console.log(`${version}\t100%`)
-    }
+    progressListener?.update(downloaded)
   } catch (err) {
     // TODO delete directory
     // TODO delete zip file
--- docs/src/index.js
+++ docs/src/index.js
@@ -1,3 +1,4 @@
+import cliProgress from "cli-progress"
 import fs from "fs/promises"
 import download from "./download"
 import { metadata } from "../metadata/all"
@@ -8,30 +9,75 @@ const piscina = new Piscina({
   filename: "./src/asciidoc-worker.js"
 })
 
+const multibar = new cliProgress.MultiBar({
+  autopadding: true,
+  format: function (options, params, payload) {
+    if (payload.asciidoc) {
+      return cliProgress.Format.Formatter({
+        ...options,
+        format: "{bar} {percentage}% | {value}/{total} | {message}"
+      }, params, payload)
+    } else {
+      return cliProgress.Format.Formatter({
+        ...options,
+        format: "{bar} {percentage}% | {value} MB/{total} MB | {message}",
+        formatValue: function (v, options, type) {
+          if (type === "total" || type === "value") {
+            return cliProgress.Format.ValueFormat(Math.round(v / 1024 / 1024), options, type)
+          }
+          return cliProgress.Format.ValueFormat(v, options, type)
+        }
+      }, params, payload)
+    }
+  }
+}, cliProgress.Presets.rect)
+
 async function main() {
   const limit = pLimit(4)
 
   let totalMessages = 0
   let asciidoctorLog = await fs.open("asciidoctor.log", "w")
+  let asciidoctorBar = multibar.create(metadata.length, 0, {
+    message: "Compile Asciidoc", asciidoc: true })
+  let asciidocProgress = 0
+
+  async function run(version) {
+    let bar
+    await limit(() => {
+      return download(version, {
+        start: (total) => {
+          bar = multibar.create(total, 0, { message: version })
+        },
+        update: (value) => {
+          bar.update(value)
+        }
+      })
+    })
+    if (bar !== undefined) {
+      bar.stop()
+      multibar.remove(bar)
+    }
+
+    let messages = await piscina.run({ version: version })
+    asciidocProgress++
+    asciidoctorBar.update(asciidocProgress)
+    totalMessages += messages.length
+    await asciidoctorLog.write(messages.join("\n"))
+  }
 
   try {
     let promises = []
     for (let m of metadata) {
-      promises.push(
-        limit(() => download(m.version))
-          .then(() => piscina.run({ version: m.version }))
-          .then(messages => {
-            if (messages.length > 0) {
-              totalMessages += messages.length
-              return asciidoctorLog.write(messages.join("\n"))
-            }
-          })
-      )
+      promises.push(run(m.version))
     }
     await Promise.all(promises)
   } finally {
     await asciidoctorLog.close()
   }
+
+  asciidoctorBar.stop()
+  multibar.remove(asciidoctorBar)
+  multibar.stop()
 
   if (totalMessages > 0) {
     console.log(`There were ${totalMessages} messages from Asciidoctor. ` +
