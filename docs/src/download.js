da004d668a4205b6fef3f6e86cfa8c26c13dd28c
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2021-11-14 15:57:26 +0100

    Do not overwrite existing zip files and extracted docs

--- docs/src/download.js
+++ docs/src/download.js
@@ -1,8 +1,10 @@
 const fetch = require("node-fetch").default
 const fs = require("fs/promises")
+const fsSync = require("fs")
 const path = require("path")
 const stream = require("stream")
 const unzipper = require("unzipper")
+const Writer = require("fstream").Writer
 
 async function download(version, progressListener) {
   // TODO handle snapshots
@@ -14,18 +16,33 @@ async function download(version, progressListener) {
   await fs.mkdir(extractedPath, { recursive: true })
   await fs.mkdir(downloadPath, { recursive: true })
 
-  let res = await fetch(url)
-  let downloaded = 0
-  let length = +res.headers.get("content-length")
+  let zipFilePath = path.join(downloadPath, `vertx-stack-docs-${version}-docs.zip`)
+  let downloadFile = false
+  let zipFile
+  let length
+  let readStream
+  try {
+    let stat = await fs.stat(zipFilePath)
+    length = stat.size
+    zipFile = fsSync.createReadStream(zipFilePath)
+    readStream = zipFile
+  } catch (e) {
+    downloadFile = true
+    zipFile = await fs.open(zipFilePath, "w")
+  }
+
   let lastProgress = 0
+  let downloaded = 0
+  if (downloadFile) {
+    let res = await fetch(url)
+    length = +res.headers.get("content-length")
+    readStream = res.body
+  }
 
   progressListener?.start(length)
 
-  // TODO do we really need to save the zip file? Maybe enable this with a flag
-  let zipFile = await fs.open(path.join(downloadPath, `vertx-stack-docs-${version}-docs.zip`), "w")
-
   try {
-    await res.body
+    await readStream
       .pipe(stream.Transform({
         transform(chunk, enc, cb) {
           downloaded += chunk.length
@@ -34,10 +51,41 @@ async function download(version, progressListener) {
             lastProgress = downloaded
           }
           this.push(chunk)
-          zipFile.write(chunk).then(() => cb()).catch(cb)
+          if (downloadFile) {
+            zipFile.write(chunk).then(() => cb()).catch(cb)
+          } else {
+            cb()
+          }
         }
       }))
-      .pipe(unzipper.Extract({ path: extractedPath }))
+      .pipe(unzipper.Parse())
+      .on("entry", function (entry) {
+        if (entry.type === "Directory") {
+          return entry.autodrain
+        }
+
+        let destPath = path.join(extractedPath, entry.path)
+
+        return fs.stat(destPath)
+          .catch(_ => null)
+          .then(stat => {
+            if (stat === null ||
+                (entry.vars.uncompressedSize > 0 && stat.size !== entry.vars.uncompressedSize) ||
+                +stat.mtime !== +entry.vars.lastModifiedDateTime) {
+              let writer = Writer({ path: destPath })
+              return new Promise((resolve, reject) => {
+                entry.pipe(writer)
+                  .on("error", reject)
+                  .on("close", resolve)
+              }).then(() => {
+                return fs.utimes(destPath, entry.vars.lastModifiedDateTime,
+                  entry.vars.lastModifiedDateTime)
+              })
+            } else {
+              return entry.autodrain()
+            }
+          })
+      })
       .promise()
     await zipFile.close()
     progressListener?.update(downloaded)
