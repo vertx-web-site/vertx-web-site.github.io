46b069094a46c90d4063522dbbc2b8b65eee7e84
Yin Hang <pigbayspy@gmail.com>
2021-11-16 16:47:56 +0800

    完成【vertx-auth-opt】的翻译和校对

--- docs/metadata/4.2.0.jsx
+++ docs/metadata/4.2.0.jsx
@@ -5,28 +5,28 @@ import { insert } from "./helpers"
 const docs = clone(oldDocs)
 
 insert(docs, "vertx-mssql-client",
-     {
-         id: "vertx-oracle-client",
-         name: "Oracle Client",
-         description: "The Reactive Oracle client.",
-         category: "databases",
-         href: "/vertx-oracle-client/java/",
-         repository: "https://github.com/eclipse-vertx/vertx-sql-client",
-         edit: "https://github.com/eclipse-vertx/vertx-sql-client/tree/master/vertx-oracle-client/src/main/asciidoc",
-         label: "Technical Preview"
-       }
- )
+  {
+    id: "vertx-oracle-client",
+    name: "Oracle Client",
+    description: "The Reactive Oracle client.",
+    category: "databases",
+    href: "/vertx-oracle-client/java/",
+    repository: "https://github.com/eclipse-vertx/vertx-sql-client",
+    edit: "https://github.com/eclipse-vertx/vertx-sql-client/tree/master/vertx-oracle-client/src/main/asciidoc",
+    label: "Technical Preview"
+  }
+)
 
 insert(docs, "vertx-auth-webauthn",
   {
     id: "vertx-auth-otp",
     name: "OTP Auth",
-    description: "One Time Password (Multi-Factor Authentication) implementation.",
+    description: "一次性密码 (多重因子身份验证) 的实现。",
     category: "authentication-and-authorization",
     href: "/vertx-auth-otp/java/",
     repository: "https://github.com/vert-x3/vertx-auth/tree/master/vertx-auth-otp",
     edit: "https://github.com/vert-x3/vertx-auth/tree/master/vertx-auth-otp/src/main/asciidoc"
   }
- )
+)
 
 export default docs
