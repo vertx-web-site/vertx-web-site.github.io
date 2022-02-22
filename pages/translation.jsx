aea546634ec155fef8b8c6c4122f86e8dca9b7a8
okou19900722 <ilayan19900722@gmail.com>
2022-02-22 09:58:54 +0800

    修复translation中导包的问题

--- pages/translation.jsx
+++ pages/translation.jsx
@@ -114,7 +114,7 @@ export async function getStaticProps() {
   const CACHE_PATH = "./.cache/community"
 
   const { Octokit } = require("@octokit/rest")
-  const { CachedFetch } = require("../components/lib/cached-fetch")
+  const { CachedFetch } = await import("../components/lib/cached-fetch")
 
   const fetch = CachedFetch({
     cacheTimeoutSeconds: CACHE_TIMEOUT_SECONDS,
