727e986a71d35134fd85d7e468b814dd0e43cf50
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2022-07-25 18:26:37 +0200

    Don't display two tooltips

--- components/SimpleIcon.jsx
+++ components/SimpleIcon.jsx
@@ -1,8 +1,8 @@
-const SimpleIcon = ({ icon, color = "currentColor", size = 24, title, ...others }) => {
+const SimpleIcon = ({ icon, color = "currentColor", size = 24, title = icon.title, ...others }) => {
   return (
     <svg role="img" width={size} height={size} viewBox="0 0 24 24"
         fill="currentColor" xmlns="http://www.w3.org/2000/svg" {...others}>
-      <title>{title || icon.title || ""}</title>
+      <title>{title || ""}</title>
       <path d={icon.path} />
     </svg>
   )
