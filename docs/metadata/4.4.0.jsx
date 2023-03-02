d7b74dd1938cc9a69ab029558ffc5c4bf2cdd1f9
Julien Viet <julien@julienviet.com>
2023-03-02 09:26:28 +0100

    Revert "Adding io_uring incubator"
    This reverts commit b09372b57b46da186d3b362e47dc2df3a3698757.

--- docs/metadata/4.4.0.jsx
+++ docs/metadata/4.4.0.jsx
@@ -37,17 +37,4 @@ insert(docs, "vertx-web-openapi-router",
   }
  )
 
- insert(docs, "vertx-io_uring-incubator",
-  {
-    id: "vertx-web-openapi-router",
-    name: "Vert.x io_uring",
-    description: "Vert.x I/O engine based on io_uring functionality.",
-    category: "core",
-    href: "/vertx-io_uring-incubator/java/",
-    repository: "https://github.com/vert-x3/vertx-io_uring-incubator",
-    edit: "https://github.com/vert-x3/vertx-io_uring-incubator/tree/main/src/main/asciidoc",
-    label: "Incubator"
-  }
- )
-
 export default docs
