344d442f291412c98841fa5495abc56615c6e6de
Julien Viet <julien@julienviet.com>
2023-11-16 10:09:45 +0100

    Better 4.5.0 metadata

--- docs/metadata/4.5.0.jsx
+++ docs/metadata/4.5.0.jsx
@@ -1,6 +1,14 @@
-import { clone } from "./helpers"
+import { clone, find } from "./helpers"
 import oldDocs from "./4.4.0"
 
 const docs = clone(oldDocs)
+
+// remove old entries
+
+// update tech previews
+
+delete find(docs, "vertx-mssql-client").label
+
+// new entries
 
 export default docs
