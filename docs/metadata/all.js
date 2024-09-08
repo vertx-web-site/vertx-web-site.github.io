04166648c7731d7a915b6c4429390b45365de173
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2024-09-08 17:14:37 +0200

    Fix webpack error

--- docs/metadata/all.js
+++ docs/metadata/all.js
@@ -8,13 +8,13 @@ function filenameToVersion(filename) {
 const metadataModules = require.context(".", false, /\.jsx$/)
 
 // convert metadata file names to versions
-const versions = metadataModules.keys()
+export const versions = metadataModules.keys()
   .map(filenameToVersion)
   .sort()
   .reverse()
 
 // read metadata
-const metadata = metadataModules.keys().map(m => {
+export const metadata = metadataModules.keys().map(m => {
   let version = filenameToVersion(m)
   return { version, metadata: metadataModules(m).default }
 }).sort((a, b) => {
@@ -24,16 +24,10 @@ const metadata = metadataModules.keys().map(m => {
 })
 
 // get latest release
-let latestRelease
+export let latestRelease
 for (let i = metadata.length - 1; i >= 0; --i) {
   if (!metadata[i].metadata.prerelease) {
     latestRelease = metadata[i]
     break
   }
-}
-
-module.exports = {
-  metadata,
-  versions,
-  latestRelease
 }
