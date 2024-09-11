da3d853882cc83f190ab9f298f6182715e3ac105
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2024-09-11 08:26:01 +0200

    Store selected version in page session

--- components/hooks/useVersion.tsx
+++ components/hooks/useVersion.tsx
@@ -1,12 +1,21 @@
 import { latestRelease } from "@/docs/metadata/all"
 import { create } from "zustand"
+import { createJSONStorage, persist } from "zustand/middleware"
 
 interface VersionState {
   version: string
   setVersion: (version: string) => void
 }
 
-export const useVersion = create<VersionState>()(set => ({
-  version: latestRelease.version,
-  setVersion: version => set(_ => ({ version })),
-}))
+export const useVersion = create<VersionState>()(
+  persist(
+    set => ({
+      version: latestRelease.version,
+      setVersion: version => set(_ => ({ version })),
+    }),
+    {
+      name: "vertx-version",
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
+)
