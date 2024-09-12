9b23aca327b145dc341b34c3ccefbbbd9898b43d
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2024-09-12 07:29:11 +0200

    Add redirects for get-started and intro-to-reactive pages

---
+++ app/get-started/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from "next/navigation"
+
+const GetStarted = () => {
+  redirect("/docs")
+}
+
+export default GetStarted
---
+++ app/introduction-to-vertx-and-reactive/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from "next/navigation"
+
+const IntroductionToVertxAndReactive = () => {
+  redirect("/docs/intro-to-reactive")
+}
+
+export default IntroductionToVertxAndReactive
