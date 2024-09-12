f31993c7b70f55ca59d2d453ed51b96532eb1134
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2024-09-12 15:04:28 +0200

    Add redirects for channels and FAQ

---
+++ app/channels/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from "next/navigation"
+
+const Channels = () => {
+  redirect("/resources/#channels")
+}
+
+export default Channels
---
+++ app/faq/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from "next/navigation"
+
+const Faq = () => {
+  redirect("/docs/faq")
+}
+
+export default Faq
