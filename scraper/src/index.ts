5cb7f3604f9adacd5154aff0eb26568dddf4e76c
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2024-09-22 12:50:06 +0200

    Parallelize scraper

--- scraper/src/index.ts
+++ scraper/src/index.ts
@@ -1,131 +1,20 @@
-import * as cheerio from "cheerio"
-import { metadata, versions } from "../../docs/metadata/all"
+import { versions } from "../../docs/metadata/all"
 import { filterLatestBugfixVersions } from "../../docs/metadata/helpers"
-import fs from "fs/promises"
-import { convert } from "html-to-text"
-import os from "os"
-import pMap from "p-map"
+import Piscina from "piscina"
 
-interface Entry {
-  slug: string
-  body: string
-}
-
-function nodeToString($node: cheerio.Cheerio<cheerio.Element>) {
-  let text = convert($node.html()!, {
-    wordwrap: false,
-    selectors: [
-      { selector: "a", options: { ignoreHref: true } },
-      { selector: "img", format: "skip" },
-      {
-        selector: "dt",
-        format: "inlineSurround",
-        options: {
-          suffix: ": ",
-        },
-      },
-      {
-        selector: "dd",
-        format: "inlineSurround",
-        options: {
-          suffix: "\n",
-        },
-      },
-      { selector: "pre", format: "skip" },
-      { selector: "td", format: "inlineSurround", options: { suffix: " " } },
-      {
-        selector: "ul",
-        format: "unorderedList",
-        options: { itemPrefix: "• " },
-      },
-    ],
-  })
-
-  // cleanup
-  text = text.replace(/\r/gm, "").replace(/^\n+/gm, "").trim()
-
-  return text
-}
+const piscina = new Piscina({
+  filename: "./dist/worker.js",
+})
 
 async function main() {
   let filteredVersions = filterLatestBugfixVersions(versions)
 
-  await pMap(
-    filteredVersions,
-    async v => {
-      console.log(`Scraping documentation of version ${v} ...`)
-      return scrapeVersion(v)
-    },
-    { concurrency: Math.max(1, os.cpus().length / 2) },
-  )
-}
-
-async function scrapeVersion(version: string) {
-  let entries = metadata.find(m => m.version === version)?.metadata?.entries
-  if (entries === undefined) {
-    throw new Error(`Unable to find metadata for version ${version}`)
+  let promises: Promise<void>[] = []
+  for (let version of filteredVersions) {
+    promises.push(piscina.run({ version }))
   }
 
-  let result: Entry[] = []
-  for (let e of entries) {
-    if (/^https?:\/\//.test(e.href)) {
-      // skip external pages
-      continue
-    }
-    let slug = e.href
-    if (slug.startsWith("/")) {
-      slug = slug.substring(1)
-    }
-    if (slug.endsWith("/")) {
-      slug = slug.substring(0, slug.length - 1)
-    }
-    let f = `../out/docs/${version}/${slug}/index.html`
-    let content = await fs.readFile(f, "utf8")
-    let $ = cheerio.load(content)
-    let $main = $("main").first()
-
-    $main.find("h1").remove()
-
-    let $sections = $main.find("div[class='sect1'],div[class='sect2']")
-
-    // convert sections first
-    $sections.each((i, el) => {
-      let $n = $(el)
-      let id
-      if ($n.attr("class") === "sect1") {
-        let h = $n.find("h2").first()
-        id = h.attr("id")!
-        h.remove()
-      } else {
-        let h = $n.find("h3").first()
-        id = h.attr("id")!
-        h.remove()
-      }
-      let s = nodeToString($n)
-      result.push({
-        slug: `${slug}#${id}`,
-        body: s,
-      })
-    })
-
-    // remove all sections
-    $sections.remove()
-
-    // now convert page
-    let s = nodeToString($main)
-    result.push({
-      slug,
-      body: s,
-    })
-  }
-
-  for (let dest of ["out", "public"]) {
-    await fs.mkdir(`../${dest}/docs/${version}`, { recursive: true })
-    await fs.writeFile(
-      `../${dest}/docs/${version}/index.json`,
-      JSON.stringify(result),
-    )
-  }
+  await Promise.all(promises)
 }
 
 main().catch(console.error)
--- scraper/webpack.config.js
+++ scraper/webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require("webpack")
 
 module.exports = {
   target: "node",
-  mode: "development",
+  mode: "production",
   cache: {
     type: "filesystem",
     cacheDirectory: path.resolve(__dirname, "../.cache/scraper/webpack"),
@@ -33,5 +33,24 @@ module.exports = {
   node: {
     __filename: true,
     __dirname: true,
+  },
+  entry: {
+    // compile main module and web worker separately (i.e. create two different
+    // output modules)
+    main: path.resolve(__dirname, "./src/index.ts"),
+    worker: path.resolve(__dirname, "./src/worker.ts"),
+  },
+  output: {
+    // this is required so the main module and the web worker get different
+    // output names
+    filename: "[name].js",
+
+    // Output web worker as commonjs module and export the `default` variable
+    // This allows it to be loaded by Piscine as a web worker (with a top-level
+    // entry function)
+    library: {
+      type: "commonjs2",
+      export: "default",
+    },
   },
 }
