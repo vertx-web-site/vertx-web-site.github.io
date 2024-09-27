65e30fbeeee477983810f1385e9eef128205d50f
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2024-09-27 09:27:02 +0200

    Performance: Replace search dialog with react-aria
    This avoids forced reflow and improves performance on pages with
    a lot of elements.

--- components/search/QuickSearch.tsx
+++ components/search/QuickSearch.tsx
@@ -37,8 +37,15 @@ const QuickSearch = ({ onClick }: QuickSearchProps) => {
         <MagnifyingGlass size="1.75rem" />
       </button>
       <button
-        className="hidden h-8 w-48 cursor-pointer select-none items-center justify-between rounded border border-gray-300 bg-bg pl-3 pr-2 text-sm text-gray-600 transition-colors hover:border-gray-500 xl:flex 2xl:w-56"
-        onClick={() => setSearchDialogOpen(true)}
+        className="hidden h-8 w-48 cursor-pointer select-none items-center justify-between rounded border border-gray-300 bg-bg pl-3 pr-2 text-sm text-gray-600 transition-colors hover:border-gray-500 focus:[outline-style:none] focus-visible:outline-2 focus-visible:[outline-style:solid] xl:flex 2xl:w-56"
+        onClick={e => {
+          if (e.screenX > 0) {
+            // User used the mouse to click the button. Remove focus so it won't
+            // be focused after we've closed the dialog.
+            e.currentTarget.blur()
+          }
+          setSearchDialogOpen(true)
+        }}
       >
         Search docs ...
         {isApple !== undefined ? (
--- components/search/SearchDialog.tsx
+++ components/search/SearchDialog.tsx
@@ -1,8 +1,7 @@
 "use client"
 
-import * as Dialog from "@radix-ui/react-dialog"
-import * as VisuallyHidden from "@radix-ui/react-visually-hidden"
 import SearchDialogContent from "./SearchDialogContent"
+import { Dialog, Modal, ModalOverlay } from "react-aria-components"
 
 interface SearchDialogProps {
   open: boolean
@@ -11,39 +10,25 @@ interface SearchDialogProps {
 
 const SearchDialog = ({ open, onClose }: SearchDialogProps) => {
   return (
-    <Dialog.Root
-      open={open}
+    <ModalOverlay
+      isOpen={open}
+      isDismissable
       onOpenChange={open => {
-        if (open) {
+        if (!open) {
           onClose()
         }
       }}
+      className="fixed inset-0 z-[90] flex items-start justify-center bg-black/20 backdrop-blur-sm data-[entering]:animate-fade-in data-[exiting]:animate-fade-out dark:bg-black/50"
     >
-      <Dialog.Portal>
-        <Dialog.Overlay
-          className="fixed inset-0 z-[90] bg-black/20 backdrop-blur-sm dark:bg-black/50 [&[data-state='closed']]:animate-fade-out [&[data-state='open']]:animate-fade-in"
-          aria-hidden="true"
-        />
-        <div className="fixed inset-0 z-[100] flex items-center justify-center p-4">
-          <div className="h-full w-[40rem] max-w-full lg:h-3/4">
-            <Dialog.Content
-              className="flex max-h-full w-full flex-col rounded-lg bg-bg text-sm shadow-[rgba(0,0,0,0.2)_0px_10px_50px] dark:bg-gray-100 [&[data-state='closed']]:animate-dialog-fade-out [&[data-state='open']]:animate-dialog-fade-in"
-              onEscapeKeyDown={() => onClose()}
-              onInteractOutside={() => onClose()}
-              onPointerDownOutside={() => onClose()}
-            >
-              <VisuallyHidden.Root>
-                <Dialog.DialogTitle>Search</Dialog.DialogTitle>
-                <Dialog.Description>
-                  Search documentation ...
-                </Dialog.Description>
-              </VisuallyHidden.Root>
-              <SearchDialogContent onClose={onClose} />
-            </Dialog.Content>
-          </div>
-        </div>
-      </Dialog.Portal>
-    </Dialog.Root>
+      <Modal className="mx-4 my-4 flex max-h-[calc(100%-2rem)] w-[40rem] max-w-[calc(100%-2rem)] flex-col rounded-lg bg-bg text-sm shadow-[rgba(0,0,0,0.2)_0px_10px_50px] data-[entering]:animate-dialog-fade-in data-[exiting]:animate-dialog-fade-out dark:bg-gray-100 lg:my-32 lg:max-h-[calc(100%-16rem)]">
+        <Dialog
+          aria-label="Search documentation ..."
+          className="flex flex-col overflow-hidden"
+        >
+          <SearchDialogContent onClose={onClose} />
+        </Dialog>
+      </Modal>
+    </ModalOverlay>
   )
 }
 
